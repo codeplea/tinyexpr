dad2bbb92580eb3d4ebb3268e3d17375705bb64f
Michael Matthews <michaeldylanmatthews@gmail.com>
2022-02-17 19:50:19 -0500

    Fixes a typo that caused a compilation error when included in an arduino projec.t

--- tinyexpr.c
+++ tinyexpr.c
@@ -158,7 +158,7 @@ static double npr(double n, double r) {return ncr(n, r) * fac(r);}
 
 static const te_variable functions[] = {
     /* must be in alphabetical order */
-    {"abs", fabs,     TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    {"abs", abs,     TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"acos", acos,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"asin", asin,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"atan", atan,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
